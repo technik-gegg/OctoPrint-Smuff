3bcf9c1dd99600682c98431c528919c9d0ecc0f1
Technik Gegg <44457073+technik-gegg@users.noreply.github.com>
2020-05-25 07:31:53 +0200

    V1.1

--- octoprint_SMuFF/__init__.py
+++ octoprint_SMuFF/__init__.py
@@ -209,7 +209,7 @@ class SmuffPlugin(octoprint.plugin.SettingsPlugin,
 			return None
 
 	def extend_script_variables(self, comm_instance, script_type, script_name, *args, **kwargs):
-		self._logger.info("Var requested from: " + str(script_name))
+		#self._logger.info("Var requested from: " + str(script_name))
 		if not script_type == "gcode":
 			return None
 
